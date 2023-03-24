2f2a80510901fc33fd02f80d46fe7a0f9282be76
Manuel Fajardo <mffajardor@correo.udistrital.edu.co>
2023-03-23 21:15:06 -0500

    Create a HTML title using FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,28 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+# Inicializar FastAPI
+app = FastAPI()
+
+# Decorador, donde esta la ruta para ingresar
+@app.get('/')
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>FastAPI - Test</title>
+        </head>
+        <body>
+            <h1>Hola, esta es una página con HTML!</h1>
+        </body>
+    </html>
+    """
+
 
 def run():
     store.get_categories()
